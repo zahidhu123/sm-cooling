7e085580c9628f5dd37b9cf1a0b0a2b87ac34958
unknown <zahidmd1996@gmail.com>
2024-04-17 23:28:53 +0530

    merge file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,51 @@ import React from 'react'
 import Image1 from '../assets/Image1.jpg'
 import Image2 from '../assets/image2.jpg'
 import Image3 from '../assets/image3.jpg'
+import Imageac1 from '../assets/ac-image1.jpg'
+import Imageac2 from '../assets/ac-image2.webp'
+import Imageac3 from '../assets/ac-image3.jpg'
+import Imageac4 from '../assets/ac-image4.jpg'
+import Imageac5 from '../assets/ac-image5.jpg'
+import Imageac6 from '../assets/ac-image6.webp'
 
 
 function Home() {
+
+    const cardImage = [
+        {
+            image: Imageac1,
+            title: "Ac Repair & Service",
+            text: "We provide troubleshoot issues, perform DIY maintenance, and maximize efficiency. Save money on repairs and keep your home comfortably cool with expert advice and practical solutions."
+        },
+        {
+            image: Imageac2,
+            title: "Ac Installation",
+            text: "we provide ins and outs of AC installation, from choosing the right unit to professional installation tips. Ensure efficient cooling and enjoy a comfortable home with expert guidance and seamless service."
+        },
+        {
+            image: Imageac3,
+            title: "Ac Uninstallation",
+            text: "We provide step-by-step instructions for safely removing your air conditioning unit. We also guide best practices for disconnecting, handling. Ensure a smooth process with expert tips for hassle-free uninstallation."
+        },
+        {
+            image: Imageac4,
+            title: "Ac Gas leak fix & refilling",
+            text: "We check Ac Leak testing with nitrogen, compressed air & soap solution. We also check Technician checks leakage AMP, pressure, air flow, temp, noise level in the end to ensure proper functioning"
+        },
+        {
+            image: Imageac5,
+            title: "Top up Service",
+            text: "Our service is refrigerant boosts your AC's performance. It helps maintain optimal cooling, prevents malfunctions, and ensures your home stays comfortable. Schedule a top-up to keep your AC running smoothly all season."
+        },
+        {
+            image: Imageac6,
+            title: "All Type Service",
+            text: "Explore our wide array of services designed to cater to diverse needs. Whether it's repairs, maintenance, installations, or upgrades, we deliver reliable and efficient solutions for all your requirements."
+        }
+    ]
+
+
+
     return (
         <>
             {/* Carousel Start */}
@@ -43,72 +85,25 @@ function Home() {
                 <div className="container">
                     <h1 className='card-main-title'> Service We Offer</h1>
                     <div className="row pt-3 card-cl-body">
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image1} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">Ac Repair & Services</h5>
-                                    <p className="card-text">We provide troubleshoot issues, perform DIY maintenance, and maximize efficiency. Save money on repairs and keep your home comfortably cool with expert advice and practical solutions.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
+                        {cardImage.map((data) => {
+                            return <div className="col-lg-4 col-md-6">
+                                <div className="card service-card-wrap">
+                                    <img src={data.image} class="card-img-top card-img-wrap" alt="..." />
+                                    <div className="card-body card-body-wrap">
+                                        <h5 className="card-title">{data.title}</h5>
+                                        <p className="card-text">{data.text}</p>
+                                        <a href="/" class="btn btn-primary">Go somewhere</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image1} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">Ac Installation</h5>
-                                    <p className="card-text">we provide ins and outs of AC installation, from choosing the right unit to professional installation tips. Ensure efficient cooling and enjoy a comfortable home with expert guidance and seamless service.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image3} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">Ac Uninstallation</h5>
-                                    <p className="card-text">We provide step-by-step instructions for safely removing your air conditioning unit. We also guide best practices for disconnecting, handling. Ensure a smooth process with expert tips for hassle-free uninstallation.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image3} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">Ac Gas leak fix & refilling</h5>
-                                    <p className="card-text">We check Ac Leak testing with nitrogen, compressed air & soap solution. We also check Technician checks leakage AMP, pressure, air flow, temp, noise level in the end to ensure proper functioning</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image3} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">Top up Service</h5>
-                                    <p className="card-text">Our service is refrigerant boosts your AC's performance. It helps maintain optimal cooling, prevents malfunctions, and ensures your home stays comfortable. Schedule a top-up to keep your AC running smoothly all season.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6">
-                            <div className="card service-card-wrap">
-                                <img src={Image3} class="card-img-top card-img-wrap" alt="..." />
-                                <div className="card-body card-body-wrap">
-                                    <h5 className="card-title">All Type Service</h5>
-                                    <p className="card-text">Explore our wide array of services designed to cater to diverse needs. Whether it's repairs, maintenance, installations, or upgrades, we deliver reliable and efficient solutions for all your requirements.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
+                        })
+                        }
                     </div>
                 </div>
             </div>
+
+            {/* Card section start */}
+
 
 
 
